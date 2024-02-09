e5dbb6248cfd5a52210b9990b656f7ccee98ad75
Owen Park <you@example.com>
2024-02-08 18:51:37 -0800

    OP - added fixtures for ucsbDiningCommonsMenuItem

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,38 @@
+const restaurantFixtures = {
+    oneRestaurant:
+    [
+      {
+       "id": 1,
+        "diningCommonsCode": "Carillo",
+        "name": "Burgers and Fries",
+        "station": "Sandwiches"      
+      }
+    ],
+
+    threeRestaurants:
+    [
+        {
+            "id": 2,
+             "diningCommonsCode": "Portola",
+             "name": "Bread",
+             "station": "Bakery"    
+        },
+
+        {
+            "id": 3,
+             "diningCommonsCode": "Ortega",
+             "name": "Burrito",
+             "station": "Mexican Grill"  
+        },
+
+        {
+            "id": 4,
+             "diningCommonsCode": "DLG",
+             "name": "Pizza",
+             "station": "Pizzeria"      
+        },
+        
+    ]
+};
+
+export { restaurantFixtures };
