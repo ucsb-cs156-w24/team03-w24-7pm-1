b27b3b58413a93f0db14bc87bb4eadfa0172ee66
Gretchen Lam <gretchenlam@ucsb.edu>
2024-02-13 14:52:04 -0800

    GL - add UCSBOrg fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,38 @@
+const organizationFixtures = {
+    oneOrganization:
+    [
+      {
+        "orgCode": "VSA",
+        "orgTranslationShort": "Viet Stu Assc",
+        "orgTranslation": "Vietnamese Student Association",
+        "inactive": false  
+      }
+    ],
+
+    threeOrganizations:
+    [
+        {
+            "orgCode": "TT",
+            "orgTranslationShort": "Theta Tau",
+            "orgTranslation": "Theta Tau",
+            "inactive": false  
+        },
+
+        {
+            "orgCode": "TASA",
+            "orgTranslationShort": "Tai Amer Stu Assc",
+            "orgTranslation": "Taiwanese American Student Association",
+            "inactive": false  
+        },
+
+        {
+            "orgCode": "DEM",
+            "orgTranslationShort": "Delt Ep Mu",
+            "orgTranslation": "Delta Epsilon Mu",
+            "inactive": true    
+        },
+        
+    ]
+};
+
+export { organizationFixtures };
