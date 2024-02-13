d089fff1af55d836f65cc2363cee0c53593df8c1
Daoyijiucsb <daoyi@umail.ucsb.edu>
2024-02-12 20:56:14 -0800

    dj - Form Component for HelpRequest plus tests

--- frontend/src/tests/components/HelpRequest/HelpRequestForm.test.js
+++ frontend/src/tests/components/HelpRequest/HelpRequestForm.test.js
@@ -13,7 +13,6 @@ jest.mock('react-router-dom', () => ({
 describe("HelpRequestForm tests", () => {
 
     test("renders correctly", async () => {
-
         render(
             <Router >
                 <HelpRequestForm/>
