898dca2b66825101b4c1995fb31e2da0d8078b92
Daoyijiucsb <daoyi@umail.ucsb.edu>
2024-02-13 01:43:16 -0800

    dj - Create Page for HelpRequest plus tests and stories

--- frontend/src/main/pages/HelpRequest/HelpRequestCreatePage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestCreatePage.js
@@ -1,12 +1,50 @@
+import HelpRequestForm from "main/components/HelpRequest/HelpRequestForm";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useBackendMutation } from "main/utils/useBackend";
+import { Navigate } from 'react-router-dom';
+import { toast } from "react-toastify";
 
-export default function HelpRequestCreatePage() {
+export default function HelpRequestCreatePage({storybook=false}) {
 
-  // Stryker disable all : placeholder for future implementation
+  const objectToAxiosParams = (helpRequest) => ({
+    url: "/api/helprequest/post",
+    method: "POST",
+    params: {
+      requesterEmail: helpRequest.requesterEmail,
+      teamId: helpRequest.teamId,
+      tableOrBreakoutRoom: helpRequest.tableOrBreakoutRoom,
+      requestTime: helpRequest.requestTime,
+      explanation: helpRequest.explanation,
+      solved: helpRequest.solved
+    }
+  });
+
+  const onSuccess = (helpRequest) => {
+    toast(`New helpRequest Created - id: ${helpRequest.id} requesterEmail: ${helpRequest.requesterEmail}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/helprequest/all"]
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/helprequest" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New HelpRequest</h1>
+        <HelpRequestForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   )
---
+++ frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
@@ -0,0 +1,117 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import HelpRequestCreatePage from "main/pages/HelpRequest/HelpRequestCreatePage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
+describe("HelpRequestCreatePage tests", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    beforeEach(() => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    });
+
+    test("renders without crashing", () => {
+        const queryClient = new QueryClient();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <HelpRequestCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+
+        const queryClient = new QueryClient();
+        const helpRequest = {
+            id: 17,
+            requesterEmail: "test@ucsb.edu",
+            teamId: "w24-7pm-1",
+            tableOrBreakoutRoom: "table",
+            requestTime: "2024-02-01T12:00",
+            explanation: "Test",
+            solved: true
+        };
+
+        axiosMock.onPost("/api/helprequest/post").reply( 202, helpRequest );
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <HelpRequestCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => {
+            expect(screen.getByTestId("HelpRequestForm-requesterEmail")).toBeInTheDocument();
+        });
+
+        const requesterEmailField = screen.getByTestId("HelpRequestForm-requesterEmail");
+        const teamIdField = screen.getByTestId("HelpRequestForm-teamId");
+        const tableOrBreakoutRoomField = screen.getByTestId("HelpRequestForm-tableOrBreakoutRoom");
+        const requestTimeField = screen.getByTestId("HelpRequestForm-requestTime");
+        const explanationField = screen.getByTestId("HelpRequestForm-explanation");
+        const solvedField = screen.getByTestId("HelpRequestForm-solved");
+        const submitButton = screen.getByTestId("HelpRequestForm-submit");
+
+        fireEvent.change(requesterEmailField, { target: { value: 'test@ucsb.edu' } });
+        fireEvent.change(teamIdField, { target: { value: 'w24-7pm-1' } });
+        fireEvent.change(tableOrBreakoutRoomField, { target: { value: 'table' } });
+        fireEvent.change(requestTimeField, { target: { value: '2024-02-01T12:00' } });
+        fireEvent.change(explanationField, { target: { value: 'Test' } });
+        fireEvent.click(solvedField);
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+            "requesterEmail": "test@ucsb.edu",
+            "teamId": "w24-7pm-1",
+            "tableOrBreakoutRoom": "table",
+            "requestTime": "2024-02-01T12:00",
+            "explanation": "Test",
+            "solved": true
+        });
+
+        expect(mockToast).toBeCalledWith("New helpRequest Created - id: 17 requesterEmail: test@ucsb.edu");
+        expect(mockNavigate).toBeCalledWith({ "to": "/helprequest" });
+    });
+
+
+});
+
