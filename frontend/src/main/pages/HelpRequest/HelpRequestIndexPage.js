585a9bf79505b6a90a948766cc9d65da4dee299d
Daoyijiucsb <daoyi@umail.ucsb.edu>
2024-02-13 16:44:24 -0800

    dj - fix bug in index

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -1,6 +1,4 @@
-/*import { useBackend } from 'main/utils/useBackend';
 
-import HelpRequestTable from 'main/components/HelpRequest/HelpRequestTable';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { hasRole, useCurrentUser } from 'main/utils/currentUser';
 import { Button } from 'react-bootstrap';
@@ -17,42 +15,17 @@ export default function HelpRequestIndexPage() {
                 href="/helprequest/create"
                 style={{ float: "right" }}
             >
-                Create Help Requests
+                Create HelpRequest 
             </Button>
         )
     } 
   }
   
-  const { data: helpRequests, error: _error, status: _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/helprequest/all"],
-      { method: "GET", url: "/api/helprequest/all" },
-      []
-    );
-
   return (
     <BasicLayout>
       <div className="pt-2">
         {createButton()}
-        <h1>Help Requests</h1>
-        <HelpRequestTable helpRequests={helpRequests} currentUser={currentUser} />
-      </div>
-    </BasicLayout>
-  )
-}*/
-
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
-export default function HelpRequestIndexPage() {
-
-  // Stryker disable all : placeholder for future implementation
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
+        <h1>HelpRequest</h1>
       </div>
     </BasicLayout>
   )
