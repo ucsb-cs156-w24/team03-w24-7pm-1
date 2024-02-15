76eb54fd683164e89509dc468b6896dde6364fcf
Gretchen Lam <gretchenlam@ucsb.edu>
2024-02-15 01:06:07 -0800

    GL - add organization edit page stories

---
+++ frontend/src/stories/pages/UCSBOrganization/UCSBOrganizationEditPage.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { rest } from "msw";
+
+import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
+import { ucsbOrganizationFixtures } from 'fixtures/ucsbOrganizationFixtures';
+
+export default {
+    title: 'pages/UCSBOrganization/UCSBOrganizationEditPage',
+    component: UCSBOrganizationEditPage
+};
+
+const Template = () => <UCSBOrganizationEditPage storybook={true}/>;
+
+export const Default = Template.bind({});
+Default.parameters = {
+    msw: [
+        rest.get('/api/currentUser', (_req, res, ctx) => {
+            return res( ctx.json(apiCurrentUserFixtures.userOnly));
+        }),
+        rest.get('/api/systemInfo', (_req, res, ctx) => {
+            return res(ctx.json(systemInfoFixtures.showingNeither));
+        }),
+        rest.get('/api/ucsborganization', (_req, res, ctx) => {
+            return res(ctx.json(ucsbOrganizationFixtures.threeOrganization[0]));
+        }),
+        rest.put('/api/ucsborganization', async (req, res, ctx) => {
+            var reqBody = await req.text();
+            window.alert("PUT: " + req.url + " and body: " + reqBody);
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ],
+}
+
+
+
