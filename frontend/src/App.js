e678956b90acb3ed301bbaa1310f6783a2e929df
Zeel <zpatel@ucsb.edu>
2024-02-15 17:57:13 -0800

    zp - added placeholders for MenuItemReview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,9 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
+import MenuItemReviewIndexPage from "main/pages/MenuItemReview/MenuItemReviewIndexPage";
+import MenuItemReviewCreatePage from "main/pages/MenuItemReview/MenuItemReviewCreatePage";
+import MenuItemReviewEditPage from "main/pages/MenuItemReview/MenuItemReviewEditPage";
 
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
@@ -75,6 +78,21 @@ function App() {
               <Route exact path="/placeholder/create" element={<PlaceholderCreatePage />} />
             </>
           )
+        }
+        {
+              hasRole(currentUser, "ROLE_USER") && (
+                <>
+                  <Route exact path="/menuitemreview" element={<MenuItemReviewIndexPage />} />
+                </>
+              )
+        }
+        {
+              hasRole(currentUser, "ROLE_ADMIN") && (
+                <>
+                  <Route exact path="/menuitemreview/edit/:id" element={<MenuItemReviewEditPage />} />
+                  <Route exact path="/menuitemreview/create" element={<MenuItemReviewCreatePage />} />
+                </>
+              )
         }
       </Routes>
     </BrowserRouter>
