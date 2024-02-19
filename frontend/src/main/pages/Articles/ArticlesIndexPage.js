39803b9998612cbf6a6ec7290a2e6bca18e1b331
liamlizard <82777456+liamlizard@users.noreply.github.com>
2024-02-19 01:47:00 -0800

    Added some index page stuff

--- frontend/src/main/pages/Articles/ArticlesIndexPage.js
+++ frontend/src/main/pages/Articles/ArticlesIndexPage.js
@@ -1,14 +1,43 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import ArticlesTable from 'main/components/Articles/ArticlesTable';
+import { Button } from 'react-bootstrap';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser';
 
 export default function ArticlesIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        return (
+            <Button
+                variant="primary"
+                href="/articles/create"
+                style={{ float: "right" }}
+            >
+                Create Article 
+            </Button>
+        )
+    } 
+  }
+  
+  const { data: articles, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/articles/all"],
+      { method: "GET", url: "/api/articles/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
+        {createButton()}
+        <h1>Articles</h1>
+        <ArticlesTable articles={articles} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
